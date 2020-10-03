5475e2c0c787e90f26cfc6212c10a4d6e8444841
lambarzol <inlinz18@ya.ru>
2020-10-03 13:02:36 +0400

    fix: added code to solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if  (!array || !array.length) return 0;
+  else return Math.min(...array);
+};
 
 exports.max = function max (array) {
-  return 0;
+  if  (!array || !array.length) return 0;
+  else return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if  (!array || !array.length) return 0;
+  let sum = 0;
+  return array.reduce((sum, current) => sum + current, 0) / array.length;
 }
